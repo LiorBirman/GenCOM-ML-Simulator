b73af148d85987fdbeb79d672fc776553773cd74
BirmanLior <Lior@BirmanP.com>
2020-08-08 21:04:30 +0300

    LAST TEST

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main
 {	
 	public static void main (String args[]) throws Exception
 {			
+		// Test TEST EST
 		UI theView = new UI();
 		SimulatorManager theModel = SimulatorManager.GetInstance();
 		
