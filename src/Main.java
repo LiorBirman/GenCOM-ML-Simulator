6dd63c7178ce53b1b82c55da9a0a9c14df3e8cf1
Lior Birman <birmanlior@gmail.com>
2020-08-08 21:06:33 +0300

    Check Name & Email

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,6 @@ public class Main
 {	
 	public static void main (String args[]) throws Exception
 {			
-		// Test TEST EST
 		UI theView = new UI();
 		SimulatorManager theModel = SimulatorManager.GetInstance();
 		
